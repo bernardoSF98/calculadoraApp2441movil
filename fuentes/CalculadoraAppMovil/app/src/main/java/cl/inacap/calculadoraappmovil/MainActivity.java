4b717e3959a472a4d1f16cc6ddfcbcfaf79f362f
Bernardo Segovia F <bernardo.segoviaf@gmail.com>
2020-09-17 22:43:35 -0300

    Agregar base de la calculadora
    Obtener los datos de los EditText en el activity
    Generar el layout del activity completo
    Agregar el color de los botones
    Agregamos funcionalidad de la calculadora
    Agregar ImageView para la calculadora

--- fuentes/CalculadoraAppMovil/app/src/main/java/cl/inacap/calculadoraappmovil/MainActivity.java
+++ fuentes/CalculadoraAppMovil/app/src/main/java/cl/inacap/calculadoraappmovil/MainActivity.java
@@ -1,21 +1,43 @@
 package cl.inacap.calculadoraappmovil;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText n1Txt;
+    private EditText n2Txt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.n1Txt = findViewById(R.id.n1Txt);
+        this.n2Txt = findViewById(R.id.n2Txt);
     }
 
-    public void saludar(View v){
-        Toast.makeText(this, "Hola Mundo Pal Pedro", Toast.LENGTH_SHORT).show();
+    public void sumar(View v){
+        int n1 = Integer.parseInt(this.n1Txt.getText().toString());
+        int n2 = Integer.parseInt(this.n2Txt.getText().toString());
+        Toast.makeText(this, "La Suma Es " + (n1 + n2), Toast.LENGTH_SHORT).show();
     }
 
+    public void restar(View v){
+        int n1 = Integer.parseInt(this.n1Txt.getText().toString());
+        int n2 = Integer.parseInt(this.n2Txt.getText().toString());
+        Toast.makeText(this, "La Resta Es " + (n1 - n2), Toast.LENGTH_SHORT).show();
+    }
+
+    public void multiplicar(View v){
+        int n1 = Integer.parseInt(this.n1Txt.getText().toString());
+        int n2 = Integer.parseInt(this.n2Txt.getText().toString());
+        Toast.makeText(this, "La Multiplicacion Es " + (n1 * n2), Toast.LENGTH_SHORT).show();
+    }
+
+    public void dividir(View v){
+        int n1 = Integer.parseInt(this.n1Txt.getText().toString());
+        int n2 = Integer.parseInt(this.n2Txt.getText().toString());
+        Toast.makeText(this, "La Division Es " + (n1 / n2), Toast.LENGTH_SHORT).show();
+    }
 }
